c1efcfc01f8f5be681b6d3208a9204afe1fffdaf
HIMANSHU MISHRA <73916471+him54@users.noreply.github.com>
2023-01-11 23:10:31 +0530

    Update and rename Creates_Files_Scripting to Creates_Files_Scripting.py

--- Creates_Files_Scripting.py
+++ Creates_Files_Scripting.py
@@ -4,7 +4,7 @@ z = (r.text)
 with open('quotes.txt', 'w') as f: // quote written in text file
   for line in z.split("\n"):
     if '<span class="text" itemprop="text">'in line:
-      line = line.replace('<span class="text" itemprop="text">“', '').replace('”</span>', "")
+      line = line.replace('<span class="text" itemprop="text">“', '').replace('”</span>', "")   #replace span folder
       line = line.strip()
       f.write(line)
-      f.write('\n') 
+      f.write('\n')
