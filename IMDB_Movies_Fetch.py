c1bd59ca57a830239e1a5f6390ab2ad89faf1794
HIMANSHU MISHRA <73916471+him54@users.noreply.github.com>
2023-01-12 20:30:36 +0530

    Update IMDB_Movies_Fetch.py

--- IMDB_Movies_Fetch.py
+++ IMDB_Movies_Fetch.py
@@ -4,8 +4,8 @@ r = requests.get("https://www.imdb.com/chart/top/")
 html = r.text
 soup = BeautifulSoup(html, 'html.parser')
 t = soup.tbody
-print(t.attrs) # attributes
-tb = soup.find('tbody', {'class':'lister-list'})
+print(t.attrs) # attributes   #{'class': ['lister-list']}
+tb = soup.find('tbody', {'class':'lister-list'}) 
 t2 = tb.findAll('tr')
 for t1 in t2:
   movie_name = t1.find('td', {'class':'titleColumn'})
