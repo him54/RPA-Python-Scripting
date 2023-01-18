861738ae54eec4472c5e10e5b47a7977c760624c
HIMANSHU MISHRA <73916471+him54@users.noreply.github.com>
2023-01-18 09:16:28 +0530

    Update IMDB_Movies_Fetch.py

--- IMDB_Movies_Fetch.py
+++ IMDB_Movies_Fetch.py
@@ -8,7 +8,7 @@ print(t.attrs) # attributes   #{'class': ['lister-list']}
 tb = soup.find('tbody', {'class':'lister-list'}) 
 t2 = tb.findAll('tr')
 for t1 in t2:
-  movie_name = t1.find('td', {'class''titleColumn'})
+  movie_name = t1.find('td', {'class' : 'titleColumn'})
   print(movie_name.a.string, movie_name.span.string)
   
        #(altr)
