0cbb7dbc7e81e49a5140b37df54fb2352982a12b
HIMANSHU MISHRA <73916471+him54@users.noreply.github.com>
2023-01-29 04:54:29 +0530

    Update Record_Quotes_toscrace.py

--- Record_Quotes_toscrace.py
+++ Record_Quotes_toscrace.py
@@ -3,7 +3,7 @@ for i in range(1,11):
   url = f'https://quotes.toscrape.com/page/{i}/'
   r = requests.get(url)
   z = r.text
-  with open('quotes.txt', 'a',encoding='utf-8') as f: //  apppend value
+  with open('quotes.txt', 'a',encoding='utf-8') as f: //  apppend value)
     for line in z.split("\n"):
       if '<span class="text" itemprop="text">'in line:
         line = line.replace('<span class="text" itemprop="text">“', '').replace('”</span>', "")
