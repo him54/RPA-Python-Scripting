3c7007f544f99f19fa78eaedd9501fa625abc31a
HIMANSHU MISHRA <73916471+him54@users.noreply.github.com>
2023-01-31 15:10:30 +0530

    Update Datetime_module_2.py

--- Datetime_module_2.py
+++ Datetime_module_2.py
@@ -3,3 +3,16 @@
 # d1 = date(y,m,d)
 # print(d1)
 # print(d1.strftime('Day %w of the week.This is %A and %B is a month name'))
+
+
+
+# from datetime import*
+# d1,m1,y1 = [int(x) for x in input("Enter the date in India Timezone").split('/')]
+# date1 = date(y1,m1,d1)
+# print(d1)
+# d2,m2,y2 = [int(x) for x in input("Enter the date in India Timezone").split('/')]
+# date2 = date(y2,m2,d2)
+# print(d2)
+
+
+
