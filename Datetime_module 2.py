2c02fc8d77539457a9ceb8b06d186e60d569ac66
HIMANSHU MISHRA <73916471+him54@users.noreply.github.com>
2023-01-31 15:09:31 +0530

    Create Datetime_module 2.py

---
+++ Datetime_module 2.py
@@ -0,0 +1,5 @@
+# from datetime import*
+# d,m,y = [int(x) for x in input("Enter the date in India Timezone").split('/')]
+# d1 = date(y,m,d)
+# print(d1)
+# print(d1.strftime('Day %w of the week.This is %A and %B is a month name'))
