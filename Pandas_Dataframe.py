3ed23fa7c8cdf63eb4d341ee21cb58a065a7cec8
HIMANSHU MISHRA <73916471+him54@users.noreply.github.com>
2023-02-07 15:37:45 +0530

    Update Pandas_Dataframe.py

--- Pandas_Dataframe.py
+++ Pandas_Dataframe.py
@@ -2,7 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 import pandas as pd
 from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.options import Option
 
 driver_service = Service(executable_path='chromedriver.exe')
 driver = webdriver.Chrome(service=driver_service)
