296d41bbba3ca78e1a3f9bc5d891b8d5c96374b6
HIMANSHU MISHRA <73916471+him54@users.noreply.github.com>
2023-02-10 11:03:17 +0530

    Update Threads.py

--- Threads.py
+++ Threads.py
@@ -27,10 +27,10 @@
 # t1.start()
 # t1.join()
 
-from threading import
+from threading import*
 class MyThread(Thread):
     def __init__(self, s):
-        #super().__init__()      #super() function is used to give access to methods and properties of a parent or sibling class.
+        #super().__init__()      super() function is used to give access to methods and properties of a parent or sibling class.
                                  # The super() function returns an object that represents the parent class.
         Thread.__init__(self)
         self.s = s
