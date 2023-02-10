8daff55f22332de31f5cd825952e3c8e3a90a6f1
HIMANSHU MISHRA <73916471+him54@users.noreply.github.com>
2023-02-11 00:41:27 +0530

    Update 08-02-2023.py

--- 08-02-2023.py
+++ 08-02-2023.py
@@ -21,7 +21,7 @@ title_list=[]
 subtitle_list=[]
 link_list=[]
 #<div class="teaser__copy-container"><a class="text-anchor-wrap" href="https://www.thesun.co.uk/sport/21296749/chelsea-hire-mental-coach-blacks-winning-culture/"><h3 class="teaser__headline t-p-color">IN-OKA</h3><p class="teaser__subdeck"> Chelsea hire All Blacks' mental skills coach to help create winning culture</p></a></div>
-#*[@id="customiser-v2-13503409"]/div[9]/div/div[3]/div/div[2]
+*[@id="customiser-v2-13503409"]/div[9]/div/div[3]/div/div[2]
 
 containers = driver.find_elements(By.XPATH, value= '//div[@class="teaser__copy-container"]')
 for container in containers:
@@ -29,7 +29,7 @@ for container in containers:
     subtitle = container.find_element(By.XPATH, value='./a/p').text
     link = container.find_element(By.XPATH,value='./a').get_attribute('href')
     title_list.append(title)
-    subtitle_list.append(subtitle
+    subtitle_list.append(subtitle)
     link_list.append(link)
 
 d={'Title':title_list,'Subtitle':subtitle_list, 'Link': link_list}
