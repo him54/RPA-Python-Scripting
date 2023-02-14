d3f258db063de99c587322e3c5d1e5b21fb1a1c8
HIMANSHU MISHRA <73916471+him54@users.noreply.github.com>
2023-02-14 16:43:50 +0530

    Update Railway_Reservation.py

--- Railway_Reservation.py
+++ Railway_Reservation.py
@@ -37,3 +37,44 @@ Reservation successful! Seat number 6 assigned to Passenger 6
 Reservation successful! Seat number 7 assigned to Passenger 7
 Reservation successful! Seat number 8 assigned to Passenger 8
 Reservation successful! Seat number 9 assigned to Passenger 9
+
+
+
+ #2nd Method
+
+from threading import *
+from time import *
+
+class Railway: # If you created a class
+    def __init__(self, available): # constructor is created
+        self.available = available
+
+    def reserve(self, wanted): # Reserve Seats
+        if self.available >= wanted: # This is to check seats are available or not
+            name = current_thread().name
+            print('%d berth allowed for %s' %(wanted, name))
+            self.available = self.available - wanted # This will decrease number of seats
+        else:
+            print("Seat are not Available")
+
+
+obj = Railway(1) # available seat
+
+T1 = Thread(target=obj.reserve, args=(1,)) # Operate First Person
+T1.setName("First Person") # Set Name for T1
+T1.start() # Initiate the Thread for First Person
+
+T2 = Thread(target=obj.reserve, args=(1,)) # Operate Second Person
+T2.setName("Second Person") # Set Name for T2
+T2.start() # Initiate the Thread for Second Person
+
+TP = int(input("\nHow Many Seats Do You want to book for Third Person : "))
+T3 = Thread(target=obj.reserve, args=(TP,)) # Operate Third Person
+T3.setName("Third Person") # Set Name for T3
+T3.start() # Initiate the Thread for Third Person
+
+#setName - set your name
+#getName - Fetch your name
+
+
+
