f1ec59f143bcf79e65b98d8bed57e1e42b794679
HIMANSHU MISHRA <73916471+him54@users.noreply.github.com>
2023-02-14 16:41:37 +0530

    Update Railway_Reservation.py

--- Railway_Reservation.py
+++ Railway_Reservation.py
@@ -24,3 +24,16 @@ for t in threads:
     t.start()
 for t in threads:
     t.join()
+    
+    #OUTPUT
+    
+Reservation successful! Seat number 0 assigned to Passenger 0
+Reservation successful! Seat number 1 assigned to Passenger 1
+Reservation successful! Seat number 2 assigned to Passenger 2
+Reservation successful! Seat number 3 assigned to Passenger 3
+Reservation successful! Seat number 4 assigned to Passenger 4
+Reservation successful! Seat number 5 assigned to Passenger 5
+Reservation successful! Seat number 6 assigned to Passenger 6
+Reservation successful! Seat number 7 assigned to Passenger 7
+Reservation successful! Seat number 8 assigned to Passenger 8
+Reservation successful! Seat number 9 assigned to Passenger 9
