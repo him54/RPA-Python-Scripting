88299e0fbb4d24458a5a3dd1478e456819b68e2a
HIMANSHU MISHRA <73916471+him54@users.noreply.github.com>
2023-02-22 12:25:07 +0530

    Update Queue.py

--- Queue.py
+++ Queue.py
@@ -32,3 +32,27 @@ t1.start()
 t2.start()
 
 #Queue is having two function 1- Get() 2 - put()
+
+
+#OUTPUT
+Producing Item: 0
+Reseiving Item 0
+Producing Item: 1
+Reseiving Item 1
+Producing Item: 2
+Reseiving Item 2
+Producing Item: 3
+Reseiving Item 3
+Producing Item: 4
+Reseiving Item 4
+Producing Item: 5
+Reseiving Item 5
+Producing Item: 6
+Reseiving Item 6
+Producing Item: 7
+Reseiving Item 7
+Producing Item: 8
+Reseiving Item 8
+Producing Item: 9
+Reseiving Item 9
+Producing Item: 10
