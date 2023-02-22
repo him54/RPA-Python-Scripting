8ab7327356f85b7c79c9f8b02b259a7897fb2cd5
HIMANSHU MISHRA <73916471+him54@users.noreply.github.com>
2023-02-23 01:16:01 +0530

    Create Audio_Book.py

---
+++ Audio_Book.py
@@ -0,0 +1,17 @@
+import pyttsx3
+import PyPDF2
+from PyPDF2 import PdfReader
+book = open("Seminar Topics.pdf", "rb") #Open the PDF file in binary mode
+reader= PyPDF2.PdfReader(book)   #Read the PDF file using PyPDF2
+pages=len(reader.pages)
+print(pages)
+
+
+speaker= pyttsx3.init() #Initialize the pyttsx3 engine
+for i in range(pages):
+    page = reader.pages[i]
+    text = page.extract_text()
+    speaker.say(text) #Convert the text to speech
+
+    speaker.runAndWait() #Run the engine
+    #speaker.save_to_file(text, 'pages.mp3')
