afb9ccfcf500867d64197f4cee70605a26309310
HIMANSHU MISHRA <73916471+him54@users.noreply.github.com>
2023-02-22 12:30:10 +0530

    Update Queue.py

--- Queue.py
+++ Queue.py
@@ -8,7 +8,7 @@ class producer:
         self.q = Queue()
 
     def produce(self):
-        for i in range(11):
+        for i in range(1,11):
             print("Producing Item:", i)
             self.q.put(i)
             sleep(1)
@@ -20,18 +20,20 @@ class consumer:
         def consume(self):
             for i in range(1, 11):
                 print("Receiving Item", self.prod.q.get(i))
+                print('\n')
 
 
-pr = producer()
-cn = consumer(pr)
+p = producer()
+c = consumer(p)
 
 
-t1=Thread(target=pr.produce)
-t2=Thread(target=cn.consume)
+t1=Thread(target=p.produce)
+t2=Thread(target=c.consume)
 t1.start()
 t2.start()
 
 #Queue is having two function 1- Get() 2 - put()
+
 
 
 #OUTPUT
