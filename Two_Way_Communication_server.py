532016112832a1041987cf7e05013950ffaac338
HIMANSHU MISHRA <73916471+him54@users.noreply.github.com>
2023-03-06 12:52:50 +0530

    Create Two_Way_Communication_server.py

---
+++ Two_Way_Communication_server.py
@@ -0,0 +1,25 @@
+import socket
+host="127.0.0.1"
+port=9000
+
+s=socket.socket()
+
+s.bind((host, port))
+s.listen(1)
+
+c,addr = s.accept()
+print("client connected")
+
+
+while True:  #Server runs continuously
+    data = c.recv(1024)
+
+    if not data:
+        break
+    print("From client:"+str(data.decode())) #Recieved his data
+    response = input("Enter the responce")
+    c.send(response.encode())
+s.close()
+
+
+
